0daf6eeb09188d5ca402b9a94a5e9f8185fb074b
satter0827 <brts50nw8q@gmail.com>
2021-11-29 14:25:59 +0900

    パスを修正

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-app = Flask(__name__, static_folder='.', static_url_path='')
-@app.route('/')
 
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route('/')
 def index():
     return app.send_static_file('index.html')
 
-app.run(port=8001, debug=False)
+app.run(port=8000, debug=False)
