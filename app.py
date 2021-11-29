7eb809e9a0616afae4f1550da43fe8e5886b812e
satter0827 <brts50nw8q@gmail.com>
2021-11-29 14:29:58 +0900

    ポート番号を変更

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 def index():
     return app.send_static_file('index.html')
 
-app.run(port=8000, debug=False)
+app.run(port=8100, debug=False)
